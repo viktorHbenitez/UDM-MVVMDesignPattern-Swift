2804af9f73e06c58dcc33f7431569d00e2d650be
viktor <victorhbenitez85@hotmail.com>
2020-05-10 19:54:34 -0500

    Creating and Decoding JSON to Models

--- GoodNews/GoodNews/Services/Webservice.swift
+++ GoodNews/GoodNews/Services/Webservice.swift
@@ -9,14 +9,21 @@
 import Foundation
 
 class Webservice {
-    func getArticles(url: URL, completion: @escaping ([Any]?) -> ()) {
-      URLSession.shared.dataTask(with: url) { data, response, error in
+    func getArticles(url: URL, completion: @escaping ([Article]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
             if let error = error {
                 print(error.localizedDescription)
                 completion(nil)
-            }
-            else if let data = data {
-                print(data)
+            } else if let data = data {
+                
+                let articleList = try? JSONDecoder().decode(ArticleList.self, from: data)
+                
+                if let articleList = articleList {
+                    completion(articleList.articles)
+                }
+                print(articleList?.articles)
             }
             
         }.resume()
